e489e1650f472c21c3ef46466ddf4f7c022b0430
RenanGAS <renansakaoki@gmail.com>
2023-12-14 20:07:33 -0300

    chore: remove javadoc files

--- TwitterMQ/target/site/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"project","l":"Classifier"},{"p":"project","l":"Find"},{"p":"project","l":"ProducerQueue"},{"p":"project","l":"Subscriber"}];updateSearchResults();
